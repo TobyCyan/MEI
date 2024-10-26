74fdaeb5f321e7703bcf69c5aa31d892f00a24ab
Nicholascyx <nicholas.chongg@gmail.com>
2024-10-27 03:24:24 +0800

    Add new Item class
    Item is a Scriptable Object that can be used for future items. An Item object can be created by Create->Item->Create New Item.
    
    Created a calendar sprite just for testing.

---
+++ Assets/Scripts/Item/Item.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Item/Create New Item")]
+public class Item : ScriptableObject
+{
+    public string itemName;
+    public Sprite icon;
+    // TODO: Fill in values needed for each item
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
