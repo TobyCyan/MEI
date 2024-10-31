b106e21031b9fe8a52842ca7d26534de09ada5df
Yong Jun Xi <ernestyongjunxi@gmail.com>
2024-10-31 16:45:42 +0800

    Update CharacterEnum.cs

--- Assets/Scripts/Enum/CharacterEnum.cs
+++ Assets/Scripts/Enum/CharacterEnum.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterEnum
+public class CharacterEnum : MonoBehaviour
 {
     public enum Character
     {
