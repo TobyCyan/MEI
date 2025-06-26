a9c693971864bc072cbbf9e12cae513f53c58eaf
YongJunXi <ernestyongjunxi@gmail.com>
2025-06-26 23:30:17 +0800

    Support persistence of diary entry database across scenes

--- Assets/Scripts/Interaction/OpenDiaryInteractable.cs
+++ Assets/Scripts/Interaction/OpenDiaryInteractable.cs
@@ -5,6 +5,7 @@ using UnityEngine.Assertions;
 public class OpenDiaryInteractable : Interactable
 {
     [SerializeField] private DiaryManager _diaryManager;
+    [Header("Entry ID Should Match The Pick Up Diary Entry If Used With DiaryEntryPickUp")]
     [SerializeField] private int _openEntryId = 0;
 
     void Start()
