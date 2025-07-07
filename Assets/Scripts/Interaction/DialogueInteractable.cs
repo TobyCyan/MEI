4e9e8c256095725a43c662d9875e5c44a73a8218
YongJunXi <ernestyongjunxi@gmail.com>
2025-07-08 00:24:25 +0800

    Remove boolean and rename mannequins to the before arrangement

--- Assets/Scripts/Interaction/DialogueInteractable.cs
+++ Assets/Scripts/Interaction/DialogueInteractable.cs
@@ -9,7 +9,6 @@ using System.Collections.Generic;
 public class DialogueInteractable : Interactable
 {
     [SerializeField] private List<DialogueInfoStruct> _dialogueTextEmotionStructList;
-    [SerializeField] private bool CanCharacterMove = false;
     private DialogueActivator _activator;
 
     private void Awake()
@@ -19,21 +18,8 @@ public class DialogueInteractable : Interactable
 
     public override IEnumerator Interact()
     {
-        // Calling activate dialogue using the dialogue _activator.
-        //Debug.Log("Interactables entered");
-        //yield return StartCoroutine(_activator.ActivateDialogue(_dialogueTextEmotionStructList));
-        if (CanCharacterMove)
-        {
-            yield return StartCoroutine(_activator.ActivateDialogue(_dialogueTextEmotionStructList));
-
-        }
-        else
-        {
-            PlayerController.Instance.StopPlayerMovement();
-            yield return StartCoroutine(_activator.ActivateDialogue(_dialogueTextEmotionStructList));
-            PlayerController.Instance.ResumePlayerMovement();
-
-        }
+        PlayerController.Instance.StopPlayerMovement();
+        yield return StartCoroutine(_activator.ActivateDialogue(_dialogueTextEmotionStructList));
+        PlayerController.Instance.ResumePlayerMovement();
     }
-
 }
